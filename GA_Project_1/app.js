1e3cbbce16302265dc823cc81934bd24e41ec6a4
Sam Bevan <samueleganbevan@gmail.com>
2020-05-04 16:51:12 -0400

    completed basic game functionality using JS

--- GA_Project_1/app.js
+++ GA_Project_1/app.js
@@ -1,5 +1,131 @@
+/////////////////////////
+//=====================//
+//==== HANGMAN GAME ===//
+//=====================//
+/////////////////////////
 
+
+
+////////////////////////
+//==Global Variables==//
+////////////////////////
+
+//jQuery Variables
 const $button = $(`.button`);
+const $userStringBox = $(`.guess-container`);
+const $counters = $(`.counter`);
+
+//gameplay variables
+const words = [`SPORTS`, `SHORTS`, `HURTS`];
+const userWord = [];
+const guessedLetters = [];
+let randomWord = ``;
+let splitRandomWord;
+
+//counters
+let guessesRemaining = 5;
+let lettersRemaining;
+
+//players
+const playerOneScore = 0;
+const playerTwoScore = 0;
+const player = 1;
+
+
+
+///////////////////
+//===Functions===//
+///////////////////
+
+//resets the game 
+const resetGame = () => {
+
+}
+
+//checks player score to see if anyone won
+const checkScore = () => {
+
+}
+
+//flips the player 
+const flipPlayer = () => {
+
+}
+
+//check the status of the game
+const checkGameStatus = () => {
+    if (lettersRemaining <= 0){
+        alert(`congrats you won!`);
+    }
+
+    if (guessesRemaining <= 0){
+        alert(`sorry you lose!`)
+    }
+};
+
+//update the counters
+const updateCounters = () => {
+    $counters.empty();
+    $counters.append(`<h3>Guesses Remaining: ${guessesRemaining}</h3><h3>Letters Remaining: ${lettersRemaining}</h3>`);
+    checkGameStatus();
+};
+
+//get the random word
+const getRandomWord = () => {
+    randomWord = words[Math.floor(Math.random() * words.length)];
+};
+
+//set the user word to blank
+const setUserWord = () => {
+    splitRandomWord = randomWord.split(``);
+    for (x = 0; x < splitRandomWord.length; x++) {
+        userWord[x] = `_`;
+    }
+    $userStringBox.append(userWord);
+};
+
+//set the Counters
+const setCounters = () => {
+    lettersRemaining = splitRandomWord.length;
+    $counters.append(`<h3>Guesses Remaining: ${guessesRemaining}</h3><h3>Letters Remaining: ${lettersRemaining}</h3>`);
+};
+
+
+//actions when guess is right
+const guessIsCorrect = () => {
+    userWord[x] = splitRandomWord[x];
+    $userStringBox.empty();
+    $userStringBox.append(userWord);
+    updateCounters();
+};
+
+//actions when guess is wrong
+const guessIsIncorrect = () => {
+    updateCounters();
+ 
+};
+
+//check the letter that is guessed
+const checkLetter = (letter) => {
+    
+    const letterCheck = splitRandomWord.some((value, index) => {
+        return value == letter;
+    })
+
+    for (x = 0; x < splitRandomWord.length; x++){
+        if(letter == splitRandomWord[x]){
+            lettersRemaining--;
+            guessIsCorrect();
+        } 
+    }
+
+    if (letterCheck === false) {
+        guessesRemaining--;
+        guessIsIncorrect();
+    }
+
+};
+
 
 // animate the buttons
 $button.on(`click`, (event) => {
@@ -22,5 +148,16 @@ $button.on(`click`, (event) => {
         $element.text(separatedLetter);
     }, 300);
 
+    checkLetter($element.text().trim())
 
 });
+
+
+
+
+getRandomWord();
+setUserWord();
+setCounters();
+
+
+
